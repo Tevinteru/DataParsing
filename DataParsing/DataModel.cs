6df4c6c1d307ad60987de6a597b09177bd9c73d4
Tevinteru <alecksey1231@mail.ru>
2024-05-12 23:52:08 +0800

    init

---
+++ DataParsing/DataModel.cs
@@ -0,0 +1,32 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataParsing
+{
+    [Table("data")]
+    internal class DataModel : BaseModel
+    {
+        [PrimaryKey("id", false)]
+        public int Id { get; set; }
+
+        [Column("random_date")]
+        public DateTime RandomDate { get; set; }
+
+        [Column("vegetable")]
+        public string Vegetable { get; set; }
+
+        [Column("image_url")]
+        public string ImageUrl { get; set; }
+
+        [Column("boolean_random")]
+        public bool BooleanRandom { get; set; }
+
+        [Column("random_number")]
+        public int RandomNumber { get; set; }
+    }
+}
--- DataParsing/Program.cs
+++ DataParsing/Program.cs
@@ -8,8 +8,40 @@ namespace DataParsing
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static Supabase.Client supabase;
+
+        static async Task Main(string[] args)
         {
+            await InitDb();
+            await FetchData();
+            Console.ReadLine();
+        }
+        public async static Task InitDb()
+        {
+            var url = "https://hudxrvxnsohtzkjtbihr.supabase.co";
+            var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1ZHhydnhuc29odHpranRiaWhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1MjY4NjIsImV4cCI6MjAzMTEwMjg2Mn0.TK38oOK4py_DC0MltY4wO2xHAtMDR-8jMcnwKNom7fo";
+
+            var options = new Supabase.SupabaseOptions
+            {
+                AutoConnectRealtime = true
+            };
+
+
+            supabase = new Supabase.Client(url, key, options);
+            await supabase.InitializeAsync();
+            
+        }
+        public static async Task FetchData()
+        {
+
+            var result = await supabase.From<DataModel>().Get();
+            var data = result.Models;
+
+
+            foreach (var item in data)
+            {
+                await Console.Out.WriteLineAsync(item.ToString());
+            }
         }
     }
 }
